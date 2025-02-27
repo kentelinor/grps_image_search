3f93aa02215cb7c21b5bd1ff28a60da250583fe6
Elinor Kent <Kentelinor@gmail.com>
2025-02-27 09:09:55 +0200

    adding test client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,14 +1,27 @@
-# client.py
+import unittest
 import grpc
 from generated.image_search_pb2 import ImageRequest
 from generated.image_search_pb2_grpc import ImageSearchStub
 
 
-class ImageSearchClient:
-    def __init__(self, channel):
-        self.stub = ImageSearchStub(channel)
+class TestImageSearchClient(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.channel = grpc.insecure_channel('server:50051')  # Assuming server is up and running at this address
+        self.stub = ImageSearchStub(self.channel)
 
-    def search_image(self, description):
-        request = ImageRequest(description=description)
+    def test_search_image(self):
+        # Test code for the 'SearchImage' method
+        request = ImageRequest(description="small white dog")
         response = self.stub.SearchImage(request)
-        return response.image_data, response.image_url
+
+        self.assertIsNotNone(response.image_data)
+        self.assertIsInstance(response.image_data, bytes)
+        self.assertIsNotNone(response.image_url)
+
+    def tearDown(self):
+        self.channel.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
